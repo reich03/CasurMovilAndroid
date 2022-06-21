b6326c0d24fb75c1112a0437c1fa952cb257ed3d
Deiby Tovar <deiby.tovar@soytul.com>
2022-06-21 14:31:15 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import WebView from 'react-native-webview';
+import OneSignal from 'react-native-onesignal';
+OneSignal.setLogLevel(6, 0);
+OneSignal.setAppId('YOUR-ONESIGNAL-APP-ID');
+//END OneSignal Init Code
+
+//Prompt for push on iOS
+OneSignal.promptForPushNotificationsWithUserResponse(response => {
+  console.log('Prompt response:', response);
+});
+
+//Method for handling notifications received while app in foreground
+OneSignal.setNotificationWillShowInForegroundHandler(
+  notificationReceivedEvent => {
+    console.log(
+      'OneSignal: notification will show in foreground:',
+      notificationReceivedEvent,
+    );
+    let notification = notificationReceivedEvent.getNotification();
+    console.log('notification: ', notification);
+    const data = notification.additionalData;
+    console.log('additionalData: ', data);
+    // Complete with null means don't show a notification.
+    notificationReceivedEvent.complete(notification);
+  },
+);
+
+//Method for handling notifications opened
+OneSignal.setNotificationOpenedHandler(notification => {
+  console.log('OneSignal: notification opened:', notification);
+});
+
+const App: () => Node = () => {
+  return <WebView source={{uri: 'https://casurdemo.sasoftco.com/'}} />;
+};
+
+export default App;
